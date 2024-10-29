2405c3a159f5caf468ec39e3b84f8b936ae42a2a
SaqibShakil303 <mds168723@gmail.com>
2024-10-29 22:42:11 +0530

    intialized components and navbar with routing

---
+++ src/app/container/quote/detail-quotes/detail-quotes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-quotes',
+  templateUrl: './detail-quotes.component.html',
+  styleUrls: ['./detail-quotes.component.css']
+})
+export class DetailQuotesComponent {
+
+}
---
+++ src/app/container/quote/quick-quotes/quick-quotes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quick-quotes',
+  templateUrl: './quick-quotes.component.html',
+  styleUrls: ['./quick-quotes.component.css']
+})
+export class QuickQuotesComponent {
+
+}
