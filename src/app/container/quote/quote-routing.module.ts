c51a221376468ed0f19850c31076ccfd98814f52
SaqibShakil303 <mds168723@gmail.com>
2024-10-30 15:10:48 +0530

    css html and routing fixed

--- src/app/container/quote/quote-routing.module.ts
+++ src/app/container/quote/quote-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuickQuotesComponent } from './quick-quotes/quick-quotes.component';
+import { DetailQuotesComponent } from './detail-quotes/detail-quotes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path:'',
+  //   redirectTo:'LoginPIN',
+  //   pathMatch:'full'
+  // },
+  {
+    path:'quickQuote',
+    component:QuickQuotesComponent,
+    title:'login by admin'
+  },
+  {
+    path:'detailQuote',
+    component: DetailQuotesComponent,
+    title:'login by employee'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
