21e135bce9292db6d94fa36fc75f4e0abbbd4071
Amaanuddin Ahmed <ahmedamaanuddin@gmail.com>
2024-10-31 16:44:14 +0530

    made product details page

--- src/app/container/product/product.module.ts
+++ src/app/container/product/product.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { MainProductComponent } from './main-product/main-product.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 @NgModule({
   declarations: [
-    MainProductComponent
+    MainProductComponent,
+    ProductDetailsComponent
   ],
   imports: [
     CommonModule,
