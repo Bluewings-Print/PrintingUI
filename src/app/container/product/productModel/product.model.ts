5a322e8aad2e016811aeb29ebc42583bc3914de7
SaqibShakil303 <mds168723@gmail.com>
2024-11-01 12:32:02 +0530

    made services & model

---
+++ src/app/container/product/productModel/product.model.ts
@@ -0,0 +1,19 @@
+// export class Product {
+//     product_ID!: number;
+//     category_Type_ID!: number;
+//     category_ID!: number;
+//     product_Name!: string;
+//     product_Description!: string;
+//     product_IsActive!: boolean;
+//     product_Price!: number;
+//     product_Image!: string;
+//     product_isMisc!: boolean;
+//   }
+
+export interface Product {
+    id: number;
+    name: string;
+    price: number;
+    color: string;
+  }
+  
